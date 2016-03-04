97f88a249a688e69eceb2083a1709253ba999a24
Pepe Becker <pepebecker@me.com>
2016-03-04 17:34:38 +0100

    Initial Commit

---
+++ Game/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  Game
+//
+//  Created by Pepe Becker on 3/4/16.
+//  Copyright © 2016 Pepe Becker. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
---
+++ Game/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Game
+//
+//  Created by Pepe Becker on 3/4/16.
+//  Copyright © 2016 Pepe Becker. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
